0b033e917c4f372585adc96520ab8553c5f2e43f
Kimito Haruna <89391641+ThachQuang@users.noreply.github.com>
2021-11-27 02:21:47 +0700

    Thêm Mẫu Project hỗ trợ tham khảo

---
+++ MauProject/Mau_DAO/ILineItemDAO.cs
@@ -0,0 +1,14 @@
+﻿using R2S.Training.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace R2S.Training.Dao
+{
+    internal interface ILineItemDAO
+    {
+        bool AddLineItem(LineItem item);
+    }
+}
---
+++ MauProject/Mau_Query/Init_LineItem.sql
@@ -0,0 +1,8 @@
+﻿use SMS
+Insert into Lineitem (order_id, product_id,quantity,price) 
+values
+(8,1,40,6000000),
+(9,2,50,7000000),
+(10,3,60,8000000),
+(11,4,70,9000000),
+(12,5,80,10000000)
